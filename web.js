79305e70673ec4741460d9660575a6597e4af9d5
maxmk <milind.koyande@gmail.com>
2013-07-04 13:21:05 +0530

    Added web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs=require("fs"); //declared variable
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var content=fs.readFileSync("index.html","utf8"); //declared variable to read index.html in utf8 encoding
+  response.send(content.toString());	//send response in string
 });
 
 var port = process.env.PORT || 5000;
